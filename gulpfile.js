36c3f158dfd7e24f98a187909904723cfbca0928
Marco Bolis <mbolis1984@gmail.com>
2015-04-25 16:59:14 +0200

    Build automation stuff

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp')
+var bower = require('gulp-bower')
+var connect = require('gulp-connect')
+var dalek = require('gulp-dalek')
+var gcb = require('gulp-callback')
+var del = require('del')
+
+gulp.task('clean', function(done) {
+	del(['dist', 'report'], done)
+})
+
+gulp.task('clean-lib', function(done) {
+	del(['bower_components'], done)
+})
+
+gulp.task('cleanup', ['clean', 'clean-lib'])
+
+gulp.task('lib', function() {
+	return bower()
+		.pipe(gulp.dest('lib/'))
+})
+
+gulp.task('_build', function() {
+	
+})
+
+gulp.task('test', function() {
+	connect.server({
+		root : ['build'],
+		port : 18000
+	})
+
+	return gulp.src(['tests/**.js'])
+		.pipe(dalek({
+			browser : ['phantomjs'],
+			reporter : ['console', 'html']
+		}))
+		.pipe(gcb(function() {
+			connect.serverClose();
+		}))
+})
